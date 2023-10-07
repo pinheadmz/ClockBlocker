984ed62802a059f1a5e037c1a518ac24051afeec
Matthew Zipkin <pinheadmz@gmail.com>
2023-10-07 12:50:52 -0400

    remove scratch

--- scratch/add.py
+++
@@ -1,8 +0,0 @@
-import bitcoinAuth
-from bitcoinrpc import AuthServiceProxy, JSONRPCException
-
-
-rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"%(bitcoinAuth.USER,bitcoinAuth.PW))
-
-addr = rpc_connection.getnewaddress()
-print addr
--- scratch/c.py
+++
@@ -1,6 +0,0 @@
-import random
-import time
-from rgbmatrix import Adafruit_RGBmatrix
-matrix = Adafruit_RGBmatrix(32, 1)
-
-matrix.Clear()
